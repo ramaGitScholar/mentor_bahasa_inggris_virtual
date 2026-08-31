627558e8cad67780db40722a1a1aafd91b4c76a8
ramaGitScholar <ramandha.putras@upi.edu>
2026-08-31 13:09:22 +0700

    fixing function calling typo

--- src/core/llm.py
+++ src/core/llm.py
@@ -7,7 +7,7 @@ from google import genai
 # membatasi jumlah pembuatan klien hanya 1, sehingga  mengurangi pemborosan
 @lru_cache(maxsize=1)
 def get_gemini_client():
-    gemini_client = genai.client(
+    gemini_client = genai.Client(
         api_key = env.GEMINI_API_KEY
     )
     
